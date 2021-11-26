aae8eeaab1d4e36d8d789cca6a4d7255f41730e5
naman0123 <83550704+naman0123@users.noreply.github.com>
2021-11-26 18:44:53 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,18 @@
+function init()
+{
+    let username = localStorage.getItem("userName");
+    document.getElementById("login_statement").innerHTML = "Hi, " + username + "!";
+}
+
+const config = {
+  apiKey: "AIzaSyBtsi9E4yjpTa6JGyC_iB1YUOtsn2MUapw",
+  authDomain: "letschat-8d407.firebaseapp.com",
+  databaseURL: "https://letschat-8d407-default-rtdb.firebaseio.com",
+  projectId: "letschat-8d407",
+  storageBucket: "letschat-8d407.appspot.com",
+  messagingSenderId: "232370649863",
+  appId: "1:232370649863:web:c79f9d8c2eda912a26a3a2"
+};
+  
+// Initialize Firebase
+firebase.initializeApp(config);
